ac1b76519e8c75590915b0101852a32fdfff7f2a
Victor Osório <victor.perticarrari@gmail.com>
2025-09-03 00:25:20 -0300

    Fixing test

--- src/main/webui/src/app/components/notification/notification.component.spec.ts
+++ src/main/webui/src/app/components/notification/notification.component.spec.ts
@@ -1,16 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NotificationComponent } from './notification.component';
+import { NotificationService } from '../../services/notification.service';
+import { EMPTY } from 'rxjs';
 
 describe('NotificationComponent', () => {
   let component: NotificationComponent;
   let fixture: ComponentFixture<NotificationComponent>;
-
+  let notificationService: jasmine.SpyObj<NotificationService>;
   beforeEach(async () => {
+    notificationService = jasmine.createSpyObj('NotificationService', ['connect', 'listen']);
+    notificationService.listen.and.returnValue(EMPTY);
     await TestBed.configureTestingModule({
-      imports: [NotificationComponent]
+      imports: [NotificationComponent],
+      providers: [{
+        provide: NotificationService, useValue: notificationService
+      }]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(NotificationComponent);
     component = fixture.componentInstance;
